20c6ccfe89b15c83641140d336e81acf8148378b
idrisello <138646409+idrisello@users.noreply.github.com>
2024-07-10 02:21:27 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+temp=pathlib.PosixPath
+pathlib.PosixPath=pathlib.WindowsPath
+#title
+st.title('Transportni klassifikatsiya qiluvchi model')
+
+#rasmni joylash
+file=st.file_uploader('Rasm yuklash',type=['png','jpeg','jpg','SVG','jfif'])
+
+if  file:
+    st.image(file)
+    #PIL convert
+    img=PILImage.create(file)
+    #model
+    modelpath=r'C:\Users\ASUS\Desktop\Projects\myvenv\Include\transport_model.pkl'
+    model = load_learner(modelpath)
+
+
+    #prediction
+    pred,pred_id,probs=model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f"Ehtimolik: {probs[pred_id]*100:.1f}%")
+
+    #plotting
+    fig=px.bar(x=probs*100,y=model.dls.vocab)
+    st.plotly_chart(fig)
